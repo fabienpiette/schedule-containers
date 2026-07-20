5e6f2b29ea748928e4d0eed0ef747c701840c773
Fabien Piette <fab.piette@gmail.com>
2026-07-20 16:49:44 +0200

    test(web): assert log rule manager invariant

--- internal/web/wake_test.go
+++ internal/web/wake_test.go
@@ -86,7 +86,7 @@ func setupWakeTestServer(t *testing.T, mockODM *mockOnDemandService) (*Server, *
 	cfg := &config.Config{WebHost: "127.0.0.1", WebPort: 0}
 
 	dockerClient, _ := docker.NewClient("unix:///var/run/docker.sock")
-	srv := NewServer(cfg, db, dockerClient, mockSched, presetSvc, mockODM, mockODM, noopLogWatch{})
+	srv := NewServer(cfg, db, dockerClient, mockSched, presetSvc, mockODM, mockODM, &spyLogWatch{})
 	return srv, mockSched
 }
 
